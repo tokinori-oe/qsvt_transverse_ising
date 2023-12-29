5fff4e5b4940edd06fd305efcc31f6134ce1a67f
tokinorioe <irinkotironkot@gmail.com>
2023-12-29 10:41:14 +0900

    cosとsinの機能の更新

--- check_plot.py
+++ check_plot.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 from pyqsp.angle_sequence import QuantumSignalProcessingPhases
 from pyqsp.poly import PolyCosineTX, PolySineTX, TargetPolynomial
 
@@ -29,13 +30,53 @@ def AngListForSineInQSP(time: float, epsilon: float) -> list[float]:
     
     return ang_seq
 
-def TransformIntoCosOfChebyshev(time: float, epsilon: float, var_list: list[float]) -> list[float]:
-    """Cosをチェビシェフ級数展開した多項式の値のリストを出力する"""
-    pass
+def TransformIntoCosOfChebyshev(time: float, epsilon: float, x_list: list[float]) -> list[float]:
+    """Cosをチェビシェフ級数展開した多項式の値のリストを出力する
+    
+    Keyword arguments:
+    time: cos(tx)のt
+    epsilon:cos(tx)を多項式近似するときの精度
+    x_list:xのlist
+    
+    Returns:
+    cos(tx)の近似多項式の値のリスト
+    """
+    r = scipy.optimize.fsolve(lambda r: (
+        np.e * np.abs(time) / (2 * r))**r - (5 / 4) * epsilon, time)[0]
+    R = np.floor(r / 2).astype(int)
+    R = max(R, 1)
+    TransformedValue = scipy.special.jv(0, time)
+    answer_list = []
+    for x in x_list:
+        for k in range(1, R+1):
+            TransformedValue += 2 * ( pow(-1, k) * scipy.special.jv(2 * k, time) * \
+                                    np.polynomial.chebyshev.chebval(x, [0] * (2 * k) + [1]))
+        answer_list.append(TransformedValue)
+    return answer_list
 
-def TransformIntoSineOfChebyshev(time: float, epsilon: float, var_list: list[float]) -> list[float]:
-    """Sineをチェビシェフ級数展開した多項式の値のリストを出力する"""
-    pass
+def TransformIntoSineOfChebyshev(time: float, epsilon: float, x_list: list[float]) -> list[float]:
+    """Sineをチェビシェフ級数展開した多項式の値のリストを出力する
+    
+    Keyword arguments:
+    time: sin(tx)のt
+    epsilon: sin(tx)を多項式近似する時の精度
+    x_list: xのlist
+    
+    Returns:
+    sin(tx)の近似多項式の値のリスト
+    """
+    r = scipy.optimize.fsolve(lambda r: (
+        np.e * np.abs(time) / (2 * r))**r - (5 / 4) * epsilon, time)[0]
+    R = np.floor(r / 2).astype(int)
+    R = max(R, 1)
+    TransformedValue = 0
+    answer_list = []
+    for x in x_list:
+        for k in range(1, R):
+            TransformedValue += 2 * ( pow(-1, k) * scipy.special.jv(2 * k + 1, time) * \
+                                    np.polynomial.chebyshev.chebval(x, [0] * (2 * k + 1) + [1]))
+        answer_list.append(TransformedValue)
+    return answer_list
 
 def QSPGateForCos(time: float, epsilon: float, theta: float) -> Gate:
     """AngListForCosから出力した角度を用いたQSPのx回転とz回転の交互配置のGateを作る
@@ -102,6 +143,7 @@ def main():
     
     #プロット
     plt.plot(a_list, finalresult, "o")
+    plt.plost(a_list, TransformIntoCosOfChebyshev(time, epsilon, a_list))
     plt.show()
 
 
